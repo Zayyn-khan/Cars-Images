a39fda585221c0397f7672a058bdee7e11d2bebb
Muhammad Zain Khan <muhammadzaink.05@gmail.com>
2025-02-08 23:06:54 +0500

    first data

---
+++ cars.js
@@ -0,0 +1,359 @@
+
+
+let fastFood = [
+
+    {
+        href: "https://cdn.pixabay.com/photo/2021/01/01/21/09/challenger-5880009_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2021/01/01/21/09/challenger-5880009_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2015/07/20/00/57/vintage-car-852239_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2015/07/20/00/57/vintage-car-852239_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2015/07/11/23/13/mercedes-benz-841465_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2015/07/11/23/13/mercedes-benz-841465_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/10/06/18/49/mercedes-4530972_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/10/06/18/49/mercedes-4530972_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2016/03/30/20/20/auto-1291492_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2016/03/30/20/20/auto-1291492_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2023/10/23/17/03/audi-8336484_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2023/10/23/17/03/audi-8336484_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2016/11/23/17/54/audi-1854056_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2016/11/23/17/54/audi-1854056_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/10/02/13/31/motorsport-4520955_960_720.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/10/02/13/31/motorsport-4520955_960_720.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/04/08/22/03/emblem-4113111_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/04/08/22/03/emblem-4113111_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/12/06/15/17/car-4677555_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/12/06/15/17/car-4677555_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2018/03/26/09/27/car-3262034_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2018/03/26/09/27/car-3262034_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/07/06/13/10/vintage-car-7305109_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/07/06/13/10/vintage-car-7305109_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/06/03/09/50/chevrolet-4248506_960_720.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/06/03/09/50/chevrolet-4248506_960_720.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/11/28/19/56/chevrolet-7623076_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/11/28/19/56/chevrolet-7623076_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/08/18/17/53/land-rover-7395410_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/08/18/17/53/land-rover-7395410_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/01/28/16/07/range-rover-2015694_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/01/28/16/07/range-rover-2015694_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2024/07/07/21/09/ford-8879720_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2024/07/07/21/09/ford-8879720_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2016/11/22/22/31/auto-1850953_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2016/11/22/22/31/auto-1850953_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2018/03/26/09/29/car-3262045_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2018/03/26/09/29/car-3262045_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2021/02/10/06/52/lamborghini-aventador-6001096_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2021/02/10/06/52/lamborghini-aventador-6001096_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/12/04/00/17/lamborghini-huracan-2996107_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/12/04/00/17/lamborghini-huracan-2996107_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2018/01/29/14/40/bmw-30-csl-3116267_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2018/01/29/14/40/bmw-30-csl-3116267_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/04/02/07/19/bmw-4993779_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/04/02/07/19/bmw-4993779_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2015/11/30/12/50/auto-1070251_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2015/11/30/12/50/auto-1070251_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/10/21/03/48/bmw-4565066_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/10/21/03/48/bmw-4565066_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/06/30/18/09/headlight-2459090_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/06/30/18/09/headlight-2459090_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/07/12/20/21/mercedes-benz-2498264_1280.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/07/12/20/21/mercedes-benz-2498264_1280.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2023/03/03/23/20/car-7828554_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2023/03/03/23/20/car-7828554_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2019/05/23/02/21/mclaren-4223024_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2019/05/23/02/21/mclaren-4223024_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2014/10/03/16/56/headlamp-471952_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2014/10/03/16/56/headlamp-471952_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2023/11/06/19/23/car-8370291_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2023/11/06/19/23/car-8370291_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/01/16/23/15/car-6943487_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/01/16/23/15/car-6943487_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2024/01/17/12/06/car-8514314_640.png",
+
+        img: "https://cdn.pixabay.com/photo/2024/01/17/12/06/car-8514314_640.png"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/11/09/01/19/car-5725327_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/11/09/01/19/car-5725327_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2021/11/08/14/25/car-6779247_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2021/11/08/14/25/car-6779247_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2023/02/05/21/34/porsche-7770665_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2023/02/05/21/34/porsche-7770665_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/12/18/00/37/car-5840866_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/12/18/00/37/car-5840866_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/09/20/23/48/bentley-5588541_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/09/20/23/48/bentley-5588541_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2023/07/07/17/54/bentley-8113173_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2023/07/07/17/54/bentley-8113173_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2018/03/04/00/09/vehicle-3196981_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2018/03/04/00/09/vehicle-3196981_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/12/25/18/17/car-5859942_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/12/25/18/17/car-5859942_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2024/08/07/13/43/auto-8952159_640.png",
+
+        img: "https://cdn.pixabay.com/photo/2024/08/07/13/43/auto-8952159_640.png"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2024/07/03/10/35/car-8869612_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2024/07/03/10/35/car-8869612_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/12/30/05/33/car-5872493_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/12/30/05/33/car-5872493_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2018/12/10/01/31/car-3866116_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2018/12/10/01/31/car-3866116_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/12/25/18/17/car-5859945_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/12/25/18/17/car-5859945_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2020/06/06/02/00/cadillac-escalade-5264975_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2020/06/06/02/00/cadillac-escalade-5264975_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2021/01/12/14/58/audi-5911690_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2021/01/12/14/58/audi-5911690_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2021/01/11/18/00/car-5909161_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2021/01/11/18/00/car-5909161_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/07/22/19/33/car-7338816_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/07/22/19/33/car-7338816_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/12/10/20/35/car-7647694_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/12/10/20/35/car-7647694_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2017/06/06/20/56/car-2378419_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2017/06/06/20/56/car-2378419_640.jpg"
+    },
+
+    {
+        href: "https://cdn.pixabay.com/photo/2022/07/18/05/12/car-7328771_640.jpg",
+
+        img: "https://cdn.pixabay.com/photo/2022/07/18/05/12/car-7328771_640.jpg"
+    },
+
+
+
+]
+
+
+let emptyfastfood = ""
+fastFood.forEach(element => {
+    
+    emptyfastfood += `
+    
+        <div class="card m-2" style="width: 16rem; padding: 10px;">
+            <a href=${element.href} target="_blank"><img src=${element.img} class="card-img-top" alt="img"></a>
+        </div>
+
+    
+    `
+    document.getElementById("fast-food-gallery").innerHTML = emptyfastfood
+
+});
